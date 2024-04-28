f43b5da1e5fcbbcc1e190a1b2c63f70e72c6b7ec
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-04-28 18:38:23 +0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
+import CardPage from './pages/card/CardPage'
 import HomePage from './pages/home/HomePage'
+import Layout from './pages/layout/Layout'
 
 function App() {
 	return (
-		<>
-			<HomePage />
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<HomePage />} />
+					<Route path='movie/:movieId' element={<CardPage />} />
+				</Route>
+
+				<Route path='/dashboard'></Route>
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
