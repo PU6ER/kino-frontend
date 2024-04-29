e86a55b531ce6cc4c45702524cff768ebe8d5841
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-04-30 00:05:04 +0300

    feat: add popular movies list && add pagination

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,42 +1,19 @@
-import { useEffect, useState } from 'react'
-import Navbar from '../../components/navbar/Navbar'
+import { useState } from 'react'
+import Pagination from '../../components/pagination/Pagination'
+import PopularList from '../../components/popular-list/PopularList'
 import './HomePage.scss'
 
 const HomePage = () => {
-	const [isHidden, setIsHidden] = useState(false)
-	const [prevScrollY, setPrevScrollY] = useState(0)
+	const [page, setPage] = useState(1)
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const currentScrollY = window.scrollY
+	const handlePageChange = (page: number) => {
+		setPage(page)
+	}
 
-			if (currentScrollY > prevScrollY) {
-				setIsHidden(true)
-			} else {
-				setIsHidden(false)
-			}
-
-			setPrevScrollY(currentScrollY)
-		}
-
-		window.addEventListener('scroll', handleScroll)
-
-		return () => {
-			window.removeEventListener('scroll', handleScroll)
-		}
-	}, [prevScrollY])
 	return (
 		<div className='container'>
-			<div
-				className={`container__header ${
-					isHidden ? 'container__header-hidden' : ''
-				}`}
-			>
-				<Navbar />
-			</div>
-			Home Page
-			<div className='' style={{ height: '500px' }}></div>
-			<div className='' style={{ height: '500px' }}></div>
+			<PopularList page={page} />
+			<Pagination handlePageChange={handlePageChange} currentPage={page} />
 		</div>
 	)
 }
