ac72d48bcd55d70bdd9ee641a34cf6d5aed1b32e
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-04-30 00:04:11 +0300

    feat: add loader && add similar list

--- src/pages/card/CardPage.tsx
+++ src/pages/card/CardPage.tsx
@@ -1,37 +1,50 @@
 import { useParams } from 'react-router-dom'
 import { useMovie } from '../../hooks/useMovie'
 
+import { useState } from 'react'
+import SimilarList from '../../components/similar-list/SimilarList'
 import './CardPage.scss'
 
 const CardPage = () => {
 	const { movieId } = useParams()
-	const { data } = useMovie(movieId ?? '')
-	console.log(data)
+	const { data: movieData, isLoading } = useMovie(movieId ?? '')
+
 	return (
 		<div className='container'>
-			<img
-				src={`https://image.tmdb.org/t/p/w1280/${data?.backdrop_path}`}
-				width={'100%'}
-				height={''}
-				className='backdrop'
-			/>
-			<div className='container__details'>
-				<div className='container__details__text'>
-					<span className='container__details__text-title'>{data?.title}</span>
-					<div className='container__details__text-second'>
-						<span>{Math.floor(data?.vote_average * 10) / 10}</span>
-						<span>{data?.release_date.slice(0, 4)}</span>
-						{data?.genres.map(genre => (
-							<span>
-								{genre.name.charAt(0).toLocaleUpperCase() +
-									genre.name.slice(1).toLocaleLowerCase()}
+			{isLoading ? (
+				<div className='loader' />
+			) : (
+				<div className='wrapper'>
+					<img
+						src={`https://image.tmdb.org/t/p/w1280/${movieData?.backdrop_path}`}
+						width={'100%'}
+						className='backdrop'
+					/>
+
+					<div className='container__details'>
+						<div className='container__details__text'>
+							<span className='container__details__text-title'>
+								{movieData?.title}
 							</span>
-						))}
-						<span>{data?.runtime} минут</span>
+							<div className='container__details__text-second'>
+								<span className='vote'>
+									{Math.floor(movieData?.vote_average * 10) / 10}
+								</span>
+								<span>{movieData?.release_date.slice(0, 4)}</span>
+								{movieData?.genres.map(genre => (
+									<span>
+										{genre.name.charAt(0).toLocaleUpperCase() +
+											genre.name.slice(1).toLocaleLowerCase()}
+									</span>
+								))}
+								<span>{movieData?.runtime} минут</span>
+							</div>
+							<div>{movieData?.overview}</div>
+						</div>
 					</div>
-					<div>{data?.overview}</div>
 				</div>
-			</div>
+			)}
+			<SimilarList />
 		</div>
 	)
 }
