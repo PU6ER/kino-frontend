5e351c0eb0b77dd9ab795de25820ec6579a0fbfd
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-04-30 00:05:52 +0300

    feat: add top rated movies page

---
+++ src/pages/top/TopPage.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Pagination from '../../components/pagination/Pagination'
+import PopularList from '../../components/popular-list/PopularList'
+import './TopPage.scss'
+import TopList from '../../components/top-list/TopList'
+
+const TopPage = () => {
+	const [page, setPage] = useState(1)
+
+	const handlePageChange = (page: number) => {
+		setPage(page)
+	}
+
+	return (
+		<div className='container'>
+			<TopList page={page} />
+			<Pagination handlePageChange={handlePageChange} currentPage={page} />
+		</div>
+	)
+}
+
+export default TopPage
