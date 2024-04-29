ad4917ab4a69db6d40129cc3c96f1e7612c7a6f7
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-04-29 23:59:49 +0300

    feat: update img

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,15 +10,16 @@ const Card = ({ movie }: ICardProps) => {
 		<div className='card'>
 			<Link to={`/movie/${movie.id}`} className='link'>
 				<img
+					key={`poster-${movie.id}`}
 					src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
 					className='card__poster'
 					width={300}
 					height={400}
 				/>
+
 				<span className='vote'>{Math.floor(movie.vote_average * 10) / 10}</span>
 			</Link>
 			<div className='card__footer'>
-				
 				<div className=''>
 					<div className='title-wrapper'>
 						<span className='title'>{movie.title}</span>
