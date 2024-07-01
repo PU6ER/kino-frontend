979d5f9f3665542bbaac4bdfb116b192772d9024
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-07-01 20:49:05 +0300

    feat: change movie api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
 import axios, { CreateAxiosDefaults } from 'axios'
 
 const options: CreateAxiosDefaults = {
-	baseURL: 'https://api.themoviedb.org/3/',
+	baseURL: 'https://api.kinopoisk.dev/v1.4',
 	headers: {
 		accept: 'application/json',
-		Authorization:
-			'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWQ5NjQzYmRkYjdkNzBiOTI3MTBlMGY1Mzk3ZDRmOSIsInN1YiI6IjY2MmI0YTBmMDFiMWNhMDExZGUzZTk1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.peCDMi1RRkGmylZjf5YjOO8N1sIJpI2RrjDOufk7BEA',
+		'X-API-KEY': '0TERTHK-XJPM6CK-H9T3FEX-DNW8W7Q',
+		// Authorization:
+		// 	'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWQ5NjQzYmRkYjdkNzBiOTI3MTBlMGY1Mzk3ZDRmOSIsInN1YiI6IjY2MmI0YTBmMDFiMWNhMDExZGUzZTk1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.peCDMi1RRkGmylZjf5YjOO8N1sIJpI2RrjDOufk7BEA',
 	},
 }
 
