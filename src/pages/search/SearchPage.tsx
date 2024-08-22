c07669fddc03e9ac0f26d7587914d260f4d2ff30
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-08-22 19:08:45 +0300

    feat: add movies list

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Card from '../../components/card/Card'
 import Pagination from '../../components/pagination/Pagination'
-import PopularList from '../../components/popular-list/PopularList'
+import { useSearch } from '../../hooks/useSearch'
 
 const SearchPage = () => {
 	const [page, setPage] = useState(1)
-
+	const { query } = useParams()
+	const { data, isLoading } = useSearch(query || '', page)
 	const handlePageChange = (page: number) => {
 		setPage(page)
 	}
 	return (
 		<div className='containerHome'>
-			<PopularList page={page} />
+			<div className='container'>
+				{isLoading ? (
+					<div className='loader' />
+				) : (
+					<div className='grid'>
+						{data &&
+							data.docs.map(movie => <Card movie={movie} key={movie.id} />)}
+					</div>
+				)}
+			</div>
 			<Pagination handlePageChange={handlePageChange} currentPage={page} />
 		</div>
 	)
