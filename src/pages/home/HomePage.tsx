f13cc8084a4cf1f07423e56ded44b87d9c97fefb
Vladislav Hasanov <64597307+PU6ER@users.noreply.github.com>
2024-08-22 19:08:32 +0300

    feat: change structure

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
+import Card from '../../components/card/Card'
 import Pagination from '../../components/pagination/Pagination'
-import PopularList from '../../components/popular-list/PopularList'
+import { usePopularMovies } from '../../hooks/usePopularMovies'
 import './HomePage.scss'
 
 const HomePage = () => {
 	const [page, setPage] = useState(1)
-
+	const { data, isLoading } = usePopularMovies(page)
 	const handlePageChange = (page: number) => {
 		setPage(page)
 	}
 
 	return (
 		<div className='containerHome'>
-			<PopularList page={page} />
+			<div className='container'>
+				{isLoading ? (
+					<div className='loader' />
+				) : (
+					<div className='grid'>
+						{data &&
+							data.docs.map(movie => <Card movie={movie} key={movie.id} />)}
+					</div>
+				)}
+			</div>
+			{/* <PopularList page={page} /> */}
 			<Pagination handlePageChange={handlePageChange} currentPage={page} />
 		</div>
 	)
