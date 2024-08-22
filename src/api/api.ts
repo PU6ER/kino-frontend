7dc96b64ad3b01cb1030f1124b8c786f683f5e77
PU6ER <djakobstv@gmail.com>
2024-08-22 12:01:22 +0300

    feat: update api

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ const options: CreateAxiosDefaults = {
 	baseURL: 'https://api.kinopoisk.dev/v1.4',
 	headers: {
 		accept: 'application/json',
-		'X-API-KEY': '0TERTHK-XJPM6CK-H9T3FEX-DNW8W7Q',
+		'X-API-KEY': '6PK06E8-WHAM6C8-PMP0SMP-MXP2NMN',
 		// Authorization:
 		// 	'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWQ5NjQzYmRkYjdkNzBiOTI3MTBlMGY1Mzk3ZDRmOSIsInN1YiI6IjY2MmI0YTBmMDFiMWNhMDExZGUzZTk1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.peCDMi1RRkGmylZjf5YjOO8N1sIJpI2RrjDOufk7BEA',
 	},
