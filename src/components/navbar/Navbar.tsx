5ffccd6bf12f8ada1b3f2525999a38b944780520
PU6ER <djakobstv@gmail.com>
2024-08-22 12:01:36 +0300

    feat: add search form

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,44 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import './Navbar.scss'
 
 const Navbar = () => {
+	const [searchQuery, setSearchQuery] = useState('')
+
+	const navigate = useNavigate()
+	const { query } = useParams()
+
+	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault()
+		setSearchQuery(e.target.value)
+	}
+	useEffect(() => {
+		if (query) {
+			setSearchQuery(query)
+		}
+	}, [query])
 	return (
 		<div className='navbar'>
-			<Link to='/'><div className='navbar__logo'>KINO</div></Link>
-			<div className='navbar__links'>
-				<Link to='/'>Популярные</Link>
-				<Link to='/top'>Лучшие</Link>
-				<Link to='/upcoming'>Ожидаемые</Link>
+			<Link to='/'>
+				<div className='navbar__logo'>KINO</div>
+				{/* <img src='../kino-logo.svg' className='navbar__logo' /> */}
+			</Link>
+			<div className='navbar__search'>
+				<form className='search-form'>
+					<input
+						type='text'
+						placeholder='Поиск...'
+						value={searchQuery}
+						onChange={e => handleSearch(e)}
+					/>
+					<button
+						type='submit'
+						className='form-button'
+						onClick={() => navigate('search/' + searchQuery)}
+					>
+						Найти
+					</button>
+				</form>
 			</div>
 			<div className=''></div>
 		</div>
