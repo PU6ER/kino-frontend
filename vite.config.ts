aeebdcfb3b42ba1b25c41d34f1025fc6bc3f5fe9
PU6ER <djakobstv@gmail.com>
2024-08-28 09:36:45 +0300

    feat: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	define: {
+		'process.env.VITE_KINOPOISK_API_KEY': JSON.stringify(
+			process.env.VITE_KINOPOISK_API_KEY
+		),
+	},
 })
