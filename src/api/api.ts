63335a662c54158f8969da0aa8134de1ef5a6e00
PU6ER <djakobstv@gmail.com>
2024-08-28 08:54:22 +0300

    feat: add env variable

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ const options: CreateAxiosDefaults = {
 	baseURL: 'https://api.kinopoisk.dev/v1.4',
 	headers: {
 		accept: 'application/json',
-		'X-API-KEY': '6PK06E8-WHAM6C8-PMP0SMP-MXP2NMN',
+		'X-API-KEY': `${import.meta.env.VITE_KINOPOISK_API_KEY}`,
 		// Authorization:
 		// 	'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWQ5NjQzYmRkYjdkNzBiOTI3MTBlMGY1Mzk3ZDRmOSIsInN1YiI6IjY2MmI0YTBmMDFiMWNhMDExZGUzZTk1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.peCDMi1RRkGmylZjf5YjOO8N1sIJpI2RrjDOufk7BEA',
 	},
